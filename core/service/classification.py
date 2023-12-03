c7e996d99977eb00a797277287276ff687206e49
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-12-03 13:18:15 +0500

    update classification service

--- core/service/classification.py
+++ core/service/classification.py
@@ -1,9 +1,10 @@
 import os
 import cv2
+import csv
 
 
 class ClassificationService():
-    def classificate(network, autograph, path_to_dataset, path_to_picture):
+    def classificate(network, autograph, path_to_dataset, path_to_picture, path_to_csv="./result/persons.csv"):
         temp_path = "./temp.png"
 
         picture = autograph.get_clear_autograph(path_to_picture)
@@ -12,4 +13,19 @@ class ClassificationService():
         classify = network.classify(temp_path, path_to_dataset)
         os.remove(temp_path)
 
-        return classify
+        csv_result = []
+        with open(path_to_csv) as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                csv_result.append(row)
+
+        def sort(item):
+            return item[1]
+
+        result = sorted(
+            zip(csv_result, classify[0]),
+            key=sort,
+            reverse=True
+        )[:3]
+
+        return result
