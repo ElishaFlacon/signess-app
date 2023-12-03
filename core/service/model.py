21d6d26d34b35cb713b7063a024c0224661331bb
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-12-03 12:02:49 +0500

    add blunt and accuracy service

--- core/service/model.py
+++ core/service/model.py
@@ -1,3 +1,6 @@
+from sklearn.metrics import roc_auc_score
+
+
 class ModelService():
     def load(network, path):
         network.load(path)
@@ -7,3 +10,15 @@ class ModelService():
 
     def train(network, dataset, epochs):
         network.train(dataset, epochs)
+
+    def blunt(network):
+        network.blunt()
+
+    def accuracy(network, dataset):
+        predicts = network.predict(dataset)
+        roc_auc = roc_auc_score(
+            y_true=dataset.target,
+            y_score=predicts.predict,
+            multi_class="ovo"
+        )
+        return roc_auc
