d765f3ab31e9fcbc53fb368cd11d5ca17b5645a8
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-12-07 21:59:42 +0500

    add find error classification files with accuracy

--- core/service/model.py
+++ core/service/model.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import roc_auc_score
+from core.utils import csv_to_array
 
 
 class ModelService():
@@ -21,4 +22,25 @@ class ModelService():
             y_score=predicts.predict,
             multi_class="ovo"
         )
-        return roc_auc
+
+        drop_count = 0
+        drop_path = "./drops.csv"
+        open(drop_path, "w")
+
+        files = csv_to_array("./result/filenames.csv")
+        files_count = len(files)
+
+        labels = dataset.class_labels
+        for i, predictArr in enumerate(predicts.predict):
+            predictIndex = max(enumerate(predictArr), key=lambda x: x[1])[0]
+            predict = labels[predictIndex]
+            target = dataset.target[i]
+
+            if predict != target:
+                drop_count += 1
+                with open(drop_path, 'a') as file:
+                    file.write(
+                        f'{files[i][0]},predict:{predict},expected:{target}\n'
+                    )
+
+        return (roc_auc, files_count, drop_count)
