8e0f461469e52583b69bd2a77cb1119d04605370
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2024-01-09 03:01:42 +0500

    update accuracy func

--- core/service/model.py
+++ core/service/model.py
@@ -1,4 +1,5 @@
-from sklearn.metrics import roc_auc_score
+import numpy
+import sklearn.metrics as sm
 from core.utils import csv_to_array
 
 
@@ -17,11 +18,52 @@ class ModelService():
 
     def accuracy(network, dataset):
         predicts = network.predict(dataset)
-        roc_auc = roc_auc_score(
-            y_true=dataset.target,
-            y_score=predicts.predict,
+        predicts_list = predicts.predict.tolist()
+
+        labels = dataset.class_labels
+
+        y_true = dataset.target
+        y_pred = []
+        for predict in predicts_list:
+            y = predict.index(max(predict))
+            y_pred.append(str(y))
+
+        accuracy = sm.accuracy_score(
+            y_true=y_true,
+            y_pred=y_pred
+        )
+
+        precision = sm.precision_score(
+            y_true=y_true,
+            y_pred=y_pred,
+            labels=labels,
+            average="micro"
+        )
+
+        recall = sm.recall_score(
+            y_true=y_true,
+            y_pred=y_pred,
+            labels=labels,
+            average="micro"
+        )
+
+        roc_auc = sm.roc_auc_score(
+            y_true=y_true,
+            y_score=predicts_list,
             multi_class="ovo"
         )
+
+        fpr, tpr, _ = sm.roc_curve(
+            y_true=y_true,
+            y_score=predicts_list
+        )
+        roc_auc_curve = sm.auc(fpr, tpr)
+
+        confusion_matrix = sm.confusion_matrix(
+            y_true=y_true,
+            y_pred=y_pred
+        )
+        confusion_matrix = numpy.flip(confusion_matrix)
 
         drop_count = 0
         drop_path = "./drops.csv"
@@ -30,11 +72,10 @@ class ModelService():
         files = csv_to_array("./result/filenames.csv")
         files_count = len(files)
 
-        labels = dataset.class_labels
-        for i, predictArr in enumerate(predicts.predict):
+        for i, predictArr in enumerate(predicts_list):
             predictIndex = max(enumerate(predictArr), key=lambda x: x[1])[0]
             predict = labels[predictIndex]
-            target = dataset.target[i]
+            target = y_true[i]
 
             if predict != target:
                 drop_count += 1
@@ -43,4 +84,14 @@ class ModelService():
                         f'{files[i][0]},predict:{predict},expected:{target}\n'
                     )
 
-        return (roc_auc, files_count, drop_count)
+        return {
+            roc_auc,
+            accuracy,
+            precision,
+            recall,
+            confusion_matrix,
+            roc_auc_curve,
+            files_count,
+            drop_count,
+            labels
+        }
